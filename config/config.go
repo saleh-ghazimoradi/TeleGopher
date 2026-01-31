21e101f42ac9b6239edf6cb002c36f3450354b20
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-01-31 14:15:47 +0330

    set postgresql configs

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v11"
+	"sync"
+	"time"
+)
+
+var (
+	instance *Config
+	once     sync.Once
+	initErr  error
+)
+
+type Config struct {
+	Server     Server
+	Postgresql Postgresql
+}
+
+type Server struct{}
+
+type Postgresql struct {
+	Host              string        `env:"POSTGRES_HOST"`
+	Port              string        `env:"POSTGRES_PORT"`
+	User              string        `env:"POSTGRES_USER"`
+	Password          string        `env:"POSTGRES_PASSWORD"`
+	Name              string        `env:"POSTGRES_NAME"`
+	TimeZone          string        `env:"POSTGRES_TIMEZONE"`
+	MaxOpenConn       int           `env:"POSTGRES_MAX_OPEN_CONN"`
+	MaxIdleConn       int           `env:"POSTGRES_MAX_IDLE_CONN"`
+	MaxIdleTime       time.Duration `env:"POSTGRES_MAX_IDLE_TIME"`
+	MaxLifetime       time.Duration `env:"POSTGRES_MAX_LIFETIME"`
+	SSLMode           string        `env:"POSTGRES_SSL_MODE"`
+	ConnectionTimeout time.Duration `env:"POSTGRES_CONNECTION_TIMEOUT"`
+}
+
+func GetConfigInstance() (*Config, error) {
+	once.Do(func() {
+		instance = &Config{}
+		initErr = env.Parse(instance)
+		if initErr != nil {
+			instance = nil
+		}
+	})
+	return instance, initErr
+}
