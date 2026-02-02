5fc481e1c9bc764a28d972cb2251992d2374ec3e
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-02 18:11:56 +0330

    add authentication DTOs

--- internal/dto/authentication.go
+++ internal/dto/authentication.go
@@ -1,11 +1,32 @@
 package dto
 
-import "github.com/saleh-ghazimoradi/TeleGopher/internal/helper"
+import (
+	"github.com/saleh-ghazimoradi/TeleGopher/internal/helper"
+	"time"
+)
 
 type RegisterRequest struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterResponse struct {
+	Id        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type LoginResponse struct {
+	User         *RegisterResponse `json:"user"`
+	AccessToken  string            `json:"access_token"`
+	RefreshToken string            `json:"refresh_token"`
 }
 
 func validateEmail(v *helper.Validator, email string) {
@@ -26,6 +47,11 @@ func validateName(v *helper.Validator, name string) {
 
 func ValidateRegisterRequest(v *helper.Validator, req *RegisterRequest) {
 	validateName(v, req.Name)
+	validateEmail(v, req.Email)
+	validatePassword(v, req.Password)
+}
+
+func ValidateLoginRequest(v *helper.Validator, req *LoginRequest) {
 	validateEmail(v, req.Email)
 	validatePassword(v, req.Password)
 }
