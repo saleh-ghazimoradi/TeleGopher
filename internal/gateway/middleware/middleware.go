b82ae542ca3517433e7b49f1e8ddc972e64ab30e
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-04-25 14:53:02 +0330

    update the middleware

--- internal/gateway/middleware/middleware.go
+++ internal/gateway/middleware/middleware.go
@@ -1,100 +1,100 @@
 package middleware
 
 import (
+	"errors"
 	"fmt"
 	"github.com/saleh-ghazimoradi/TeleGopher/config"
-	"github.com/saleh-ghazimoradi/TeleGopher/internal/domain"
 	"github.com/saleh-ghazimoradi/TeleGopher/internal/helper"
 	"github.com/saleh-ghazimoradi/TeleGopher/utils"
-	"log/slog"
 	"net/http"
 	"strings"
 )
 
 type Middleware struct {
-	cfg         *config.Config
-	logger      *slog.Logger
-	errResponse *helper.ErrResponse
+	logger utils.LoggerStrategy
+	cfg    *config.Config
 }
 
-func (m *Middleware) Logging(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (m *Middleware) Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		m.logger.Info("Incoming request: ", "method", r.Method, "path", r.URL.Path, "protocol", r.Proto, "remote_addr", r.RemoteAddr)
-		next(w, r)
-	}
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (m *Middleware) CORS(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (m *Middleware) CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Platform")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-
-		next(w, r)
-	}
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (m *Middleware) Recover(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (m *Middleware) Recover(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				w.Header().Set("Connection", "close")
-				m.errResponse.ServerErrorResponse(w, r, fmt.Errorf("%v", err))
+				helper.InternalServerError(w, "panic recovery hit", fmt.Errorf("%v", err))
 			}
 		}()
-		next(w, r)
-	}
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (m *Middleware) Authenticate(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (m *Middleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			m.errResponse.InvalidCredentialsResponse(w, r)
+			helper.UnauthorizedResponse(w, "Authorization header missing")
 			return
 		}
 
 		tokenParts := strings.Split(authHeader, " ")
 		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
-			m.errResponse.InvalidAuthenticationTokenResponse(w, r)
+			helper.UnauthorizedResponse(w, "Invalid authorization header format")
 			return
 		}
 
 		platform := r.Header.Get("X-Platform")
-		if platform != string(domain.PlatformWeb) && platform != string(domain.PlatformMobile) {
-			m.errResponse.InvalidCredentialsResponse(w, r)
+		if platform != "web" && platform != "mobile" {
+			helper.BadRequestResponse(w, "invalid platform", errors.New("invalid platform"))
 			return
 		}
 
 		claims, err := utils.ValidateToken(tokenParts[1], m.cfg.JWT.Secret)
 		if err != nil {
-			m.errResponse.InvalidAuthenticationTokenResponse(w, r)
+			helper.UnauthorizedResponse(w, "Unauthorized")
 			return
 		}
 
 		if claims.Platform != platform {
-			m.errResponse.InvalidAuthenticationTokenResponse(w, r)
-			return
+			helper.UnauthorizedResponse(w, "Unauthorized")
 		}
 
 		ctx := r.Context()
 		ctx = utils.WithUserId(ctx, claims.UserId)
-		ctx = utils.WithUserName(ctx, claims.Name)
+		ctx = utils.WithName(ctx, claims.Name)
 		ctx = utils.WithPlatform(ctx, claims.Platform)
-		next(w, r.WithContext(ctx))
-	}
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+
+	})
 }
 
-func NewMiddleware(cfg *config.Config, logger *slog.Logger, errResponse *helper.ErrResponse) *Middleware {
+func (m *Middleware) WrapAuth(handlerFunc http.HandlerFunc) http.Handler {
+	return m.Authenticate(handlerFunc)
+}
+
+func NewMiddleware(logger utils.LoggerStrategy, cfg *config.Config) *Middleware {
 	return &Middleware{
-		cfg:         cfg,
-		logger:      logger,
-		errResponse: errResponse,
+		logger: logger,
+		cfg:    cfg,
 	}
 }
